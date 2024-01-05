49a177dc56d172ca611a391ff88b4d971231542b
IvanS <i.surovtsev22@gmail.com>
2024-01-05 17:25:59 +0500

    Fix gameTitle

--- src/components/GameTitle/GameTitle.jsx
+++ src/components/GameTitle/GameTitle.jsx
@@ -1,21 +1,25 @@
 import { star } from 'assets/images';
 import classes from './GameTitle.module.css';
 
-export const GameTitle = (props) => {
+export const GameTitle = ({children, litleStar, bigStar}) => {
   return (
     <div>
       <h1 className={classes.title}>
-        <img
-          src={star}
-          alt="star"
-          className={`${classes.star} ${classes.smallStar}`}
-        />
-        <img
-          src={star}
-          alt="star"
-          className={`${classes.star} ${classes.bigStar}`}
-        />
-        Найди пару
+        {litleStar && (
+          <img
+            src={star}
+            alt="star"
+            className={`${classes.star} ${classes.smallStar}`}
+          />
+        )}
+        {bigStar && (
+          <img
+            src={star}
+            alt="star"
+            className={`${classes.star} ${classes.bigStar}`}
+          />
+        )}
+        {children ? children : "Найди пару"}
       </h1>
     </div>
   );
