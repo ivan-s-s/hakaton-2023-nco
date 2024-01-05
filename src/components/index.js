856cb0b3a7d68090afb01f21a7ef087303d44489
IvanS <i.surovtsev22@gmail.com>
2024-01-05 17:08:04 +0500

    Update a settings page

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import { Navbar } from './Navbar/Navbar.jsx';
 import { Board } from './Board/Board.jsx';
 import { Confirm } from './Confirm/Confirm.jsx';
 import { GameTitle } from './GameTitle/GameTitle.jsx';
+import { LevelButton } from './LevelButton/LevelButton.jsx';
 
-export { Counter, Navbar, Board, Confirm, GameTitle };
+export { Counter, Navbar, Board, Confirm, GameTitle, LevelButton };
