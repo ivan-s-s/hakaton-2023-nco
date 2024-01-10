ee72b4e0fb95185dfcbd57b7569d93a327bd1868
IvanS <i.surovtsev22@gmail.com>
2024-01-10 16:47:48 +0500

    Add adaptive to home page

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,5 +1,6 @@
 import startImg from 'assets/images/start.png';
 import classes from './Home.module.css';
+import { star } from 'assets/images';
 import { GameTitle } from 'components';
 
 export const Home = (props) => {
@@ -11,14 +12,26 @@ export const Home = (props) => {
       <img src={startImg} className={classes.startImg} alt="start" />
       <div className={classes.startContainer}>
         <div className={classes.startBox}>
-          <GameTitle litleStar bigStar/>
-          <div className={classes.start}>Начать игру</div>
+          <img
+            src={star}
+            alt="star"
+            className={`${classes.star} ${classes.smallStar}`}
+          />
+          <img
+            src={star}
+            alt="star"
+            className={`${classes.star} ${classes.bigStar}`}
+          />
+          <GameTitle/>
         </div>
-        <button className={classes.playBtn} onClick={onClickPlay}>
-          <div className={classes.playIcon}>
-            <div className={classes.play}></div>
-          </div>
-        </button>
+        <div className={classes.divButton}>
+          <button className={classes.playBtn} onClick={onClickPlay}>
+            <div className={classes.playIcon}>
+              <div className={classes.play}></div>
+            </div>
+          </button>
+          <div className={classes.start}>начать игру</div>
+        </div>
       </div>
     </div>
   );
