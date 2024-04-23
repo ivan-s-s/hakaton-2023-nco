beb69d587164773edf996d3d385d26e371cd7864
IvanS <i.surovtsev22@gmail.com>
2024-04-24 02:05:31 +0500

    Add a public folder

--- src/views/index.js
+++
@@ -1,6 +0,0 @@
-import { Game } from './Game/Game.jsx';
-import { Home } from './Home/Home.jsx';
-import { Settings } from './Settings/Settings.jsx';
-import { Results } from './Results/Results.jsx';
-
-export { Game, Home, Settings, Results };
