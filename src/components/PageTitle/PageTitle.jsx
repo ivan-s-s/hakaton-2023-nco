97047ec75ce9658ba405d93876ad1cbfed6ca4c0
IvanS <i.surovtsev22@gmail.com>
2024-04-24 03:04:18 +0500

    Add build files

--- src/components/PageTitle/PageTitle.jsx
+++
@@ -1,9 +0,0 @@
-import classes from './PageTitle.module.css';
-
-export const PageTitle = ({children, style, ...rest}) => {
-  return (
-    <legend style={style} className={classes.pageTitle} {...rest}>
-      {children}
-    </legend>
-  );
-};
